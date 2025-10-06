c36ff5f97ec139502e596a2e544b21b143b24d11
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2025-10-06 17:48:20 -0300

    novo socket 06/10

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("https://socket-globo.onrender.com");
+const socket = io("https://socket-globo-1.onrender.com");
 export default socket
 
 //https://socket-globo.onrender.com
