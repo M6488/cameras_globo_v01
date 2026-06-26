ecffb811b91aec80222da256454b6b6410f94b6f
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2026-06-26 19:52:13 +0000

    remove node_modules

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
-const socket = io("https://socket-globo-1.onrender.com");
+const socket = io("https://socket-globo-sklm.onrender.com");
 export default socket
 
 //https://socket-globo.onrender.com
 
 //https://speech-socket.onrender.com
+
+//https://socket-globo-1.onrender.com
