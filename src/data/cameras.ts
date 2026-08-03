b6641f70d7d3132ae9a46d5b055e51309a06aa9b
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2026-08-03 21:08:10 +0000

    mudança cameras

--- src/data/cameras.ts
+++ src/data/cameras.ts
@@ -5,7 +5,7 @@ export const cameras: Camera[] = [
         id: 1,
         keyword: 'Boa viagem',
         address: 'rua da auraro 34380',
-        url: 'http://localhost:8888/cam1_transcoded_output/index.m3u8',
+        url: 'http://localhost:8888/boa_viagem/index.m3u8',
         inDropzone: false
 
     },
@@ -13,48 +13,48 @@ export const cameras: Camera[] = [
         id: 2,
         keyword: 'BR 101',
         address: 'Avenida norte de brasil',
-        url: 'http://localhost:8888/cam6/index.m3u8'
+        url: 'http://localhost:8888/br_101/index.m3u8'
     },
     {
         id: 3,
         keyword: 'Guararapes',
         address: 'visão da rua guara',
-        url: 'http://localhost:8888/cam2/index.m3u8'
+        url: 'http://localhost:8888/guararapes/index.m3u8'
     },
     {
         id: 4,
         keyword: 'Rua da Aurora',
-        address: 'rua da aurora 4353',
-        url: 'http://localhost:8888/cam3/index.m3u8'
+        address: 'rua da aurora',
+        url: 'http://localhost:8888/rua_aurora/index.m3u8'
     },
     {
         id: 5,
         keyword: 'Derby',
-        address: 'Praça do Derby 7231',
-        url: 'http://localhost:8888/cam4/index.m3u8'
+        address: 'Praça do Derby',
+        url: 'http://localhost:8888/derby/index.m3u8'
     },
     {
         id: 6,
         keyword: 'Boa vista',
         address: 'Avenida conde da boa vista',
-        url: 'http://localhost:8888/cam5/index.m3u8'
+        url: 'http://localhost:8888/boa_vista/index.m3u8'
     },
     {
         id: 7,
         keyword: 'PE 15',
-        address: 'Pernambuco numero 15',
-        url: 'http://localhost:8888/cam7/index.m3u8'
+        address: 'PE-15',
+        url: 'http://localhost:8888/pe_15/index.m3u8'
     },
     {
         id: 8,
         keyword: 'Torre Aurora',
-        address: 'aaaa torreeee',
-        url: 'http://localhost:8888/cam8_transcoded_output/index.m3u8'
+        address: 'Antena da Globo',
+        url: 'http://localhost:8888/aurora/index.m3u8'
     },
     {
         id: 9,
         keyword: 'Caruaru',
-        address: 'Bairro de Kennedy',
+        address: 'Torre de Caruaru',
         url: 'http://localhost:8888/cam9_transcoded_output/index.m3u8'
     },
 ]
