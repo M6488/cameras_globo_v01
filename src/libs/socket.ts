502e38f84ce03fd86ce8c3a6862823697696fc31
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2026-08-07 16:01:32 +0000

    mudança socket1

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
-const socket = io("https://socket-globo-sklm.onrender.com");
+const socket = io("https://speech-socket.onrender.com");
 export default socket
-
-//https://socket-globo.onrender.com
 
 //https://speech-socket.onrender.com
 
-//https://socket-globo-1.onrender.com
+//https://socket-globo-sklm.onrender.com
