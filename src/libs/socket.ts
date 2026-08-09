596b37a79b1e23be3a8d559e5a106f69d5a6e0f7
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2026-08-09 18:54:57 +0000

    novo socketv1

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
-const socket = io("https://speech-socket.onrender.com");
+const socket = io("https://socketglobo-v1.onrender.com");
 export default socket
 
 //https://speech-socket.onrender.com
 
 //https://socket-globo-sklm.onrender.com
+
+//https://socketglobo-v1.onrender.com
