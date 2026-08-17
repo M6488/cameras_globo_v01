06b3d4edea908f42dadf0ecc7991e0fe9a01bc6b
Lucas Santiago <88990286+M6488@users.noreply.github.com>
2026-08-17 12:23:16 +0000

    socket gpu

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
-const socket = io("https://socket-globo-sklm.onrender.com");
+const socket = io("https://socket-globo-zq79.onrender.com");
 export default socket
 
 //https://speech-socket.onrender.com
 
 //https://socket-globo-sklm.onrender.com
 
-//https://socketglobo-v1.onrender.com
+//https://socket-globo-zq79.onrender.com
